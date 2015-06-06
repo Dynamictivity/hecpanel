1544a6f9bb950dd2963f52d57153ba115edfeeee
Travis Rowland <travis.rowland@dynamictivity.com>
2015-06-06 01:17:25 -0700

    Some last minute tweaks

--- app/Config/SKEL/SERVER_SCRIPTS/cron.bat
+++ app/Config/SKEL/SERVER_SCRIPTS/cron.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-call "C:\xampp\htdocs\hecpanel\app\Console\cake.bat" Instances.se_server cron -app "C:\xampp\htdocs\hecpanel\app"
+call "C:\xampp\htdocs\hecpanel\app\Console\cake.bat" Instances.se_server cron -app "C:\xampp\htdocs\hecpanel\app" > cron.log
